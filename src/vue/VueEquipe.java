53015796d80c0a3b2b304adc1bcd6aa02c72e6c1
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-09-05 18:45:00 -0400

    Ajout de afficherEquipe()
    Ajout de la fonction afficherEquipe() qui permet d'afficher une équipe rentrée en paramètre sur le panneau.

--- src/vue/VueEquipe.java
+++ src/vue/VueEquipe.java
@@ -4,31 +4,46 @@ import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import modele.Equipe;
 import javafx.scene.control.Label;
 
 public class VueEquipe extends Application {
 
+	protected Label valeurNom;
+	protected Label valeurAnneeDeCreation;
+	protected Label valeurRegion;
+	
 	@Override
 	public void start(Stage stade) throws Exception {
+		
+		
 		Pane panneau = new Pane();
 		GridPane grilleEquipe = new GridPane();
 		
-		Label valeurNom = new Label("Fnatic");
+		valeurNom = new Label("Fnatic");
 		grilleEquipe.add(new Label("Nom : "), 0, 0);
 		grilleEquipe.add(valeurNom, 1, 0);
 		
-		Label valeurAnneeDeCreation = new Label("2011");
+		valeurAnneeDeCreation = new Label("2011");
 		grilleEquipe.add(new Label("Création : "), 0, 1);
 		grilleEquipe.add(valeurAnneeDeCreation, 1, 1);
 		
-		Label valeurRegion = new Label("Europe");
+		valeurRegion = new Label("Europe");
 		grilleEquipe.add(new Label("Région : "), 0, 2);
 		grilleEquipe.add(valeurRegion, 1, 2);
 		
 		panneau.getChildren().add(grilleEquipe);
 		
- 		stade.setScene(new Scene(panneau, 400, 400));
+		
+		this.afficherEquipe(new Equipe("Gambit", "2011", "Europe"));
+ 		
+		stade.setScene(new Scene(panneau, 400, 400));
 		stade.show();
 	}
-
+	
+	public void afficherEquipe(Equipe equipe) {
+		this.valeurAnneeDeCreation.setText(equipe.getAnneeDeCreation());
+		this.valeurNom.setText(equipe.getNom());
+		this.valeurRegion.setText(equipe.getRegion());
+	}
 }
