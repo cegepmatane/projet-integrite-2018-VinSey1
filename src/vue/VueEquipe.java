03eb19b0d0f98c509167dbb1fe8ab35449bf1482
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-09-05 18:27:10 -0400

    Grille valeurs d'une équipe théorique
    Création d'un prototype d'affichage pour une équipe avec son nom, son année de création et sa région.

--- src/vue/VueEquipe.java
+++ src/vue/VueEquipe.java
@@ -1,11 +1,33 @@
 package vue;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
 public class VueEquipe extends Application {
 
 	@Override
 	public void start(Stage stade) throws Exception {
+		Pane panneau = new Pane();
+		GridPane grilleEquipe = new GridPane();
+		
+		Label valeurNom = new Label("Fnatic");
+		grilleEquipe.add(new Label("Nom : "), 0, 0);
+		grilleEquipe.add(valeurNom, 1, 0);
+		
+		Label valeurAnneeDeCreation = new Label("2011");
+		grilleEquipe.add(new Label("Création : "), 0, 1);
+		grilleEquipe.add(valeurAnneeDeCreation, 1, 1);
+		
+		Label valeurRegion = new Label("Europe");
+		grilleEquipe.add(new Label("Région : "), 0, 2);
+		grilleEquipe.add(valeurRegion, 1, 2);
+		
+		panneau.getChildren().add(grilleEquipe);
+		
+ 		stade.setScene(new Scene(panneau, 400, 400));
 		stade.show();
 	}
 
