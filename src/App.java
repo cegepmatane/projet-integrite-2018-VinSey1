0ba63d01203fef095c60376cc0704abbd8db9769
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-09-05 18:14:32 -0400

    Vue d'une Équipe
    Création de la vue d'une équipe qui va pouvoir contenir plusieurs joueurs

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import vue.VueEquipe;
+
+public class App {
+	
+	public static void main(String[] parametres) {
+		VueEquipe.launch(VueEquipe.class, parametres);
+	}
+}
---
+++ src/vue/VueEquipe.java
@@ -0,0 +1,12 @@
+package vue;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VueEquipe extends Application {
+
+	@Override
+	public void start(Stage stade) throws Exception {
+		stade.show();
+	}
+
+}
