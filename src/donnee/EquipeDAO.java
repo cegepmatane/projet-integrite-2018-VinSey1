d263cf1db4c07dd995e14500cbcd096004acd3b2
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-09-12 18:58:29 -0400

    Connexion à postgres / Correction BuildPath
    Établissement de la connexion à postgres et correction du build path, la librairie était mal implantée.

--- src/donnee/EquipeDAO.java
+++ src/donnee/EquipeDAO.java
@@ -2,7 +2,8 @@ package donnee;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import modele.Equipe;
 
 public class EquipeDAO {
@@ -16,6 +17,22 @@ public class EquipeDAO {
 	}
 	
 	public List<Equipe> listerEquipes(){
+		String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
+		String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/projet-integrite-2018-VinSey1";
+		String BASEDEDONNEES_USAGER = "postgres";
+		String BASEDEDONNEES_MOTDEPASSE = "05111998";
+		
+		try {
+			Class.forName(BASEDEDONNEES_DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			DriverManager.getConnection(BASEDEDONNEES_URL, BASEDEDONNEES_USAGER, BASEDEDONNEES_MOTDEPASSE);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return this.simulerListeEquipe();
 	}
 }
